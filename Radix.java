a49bd1f4b2479b79914c20926d5629daa8d68a3f
Jun <jkubo20@stuy.edu>
2021-01-11 14:26:39 -0500

    not workign

---
+++ Radix.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+public class Radix {
+  public static int nth(int n, int col) {
+    return Math.floor(int(n / Math.pow(10, col - 1));
+  }
+  //public static int length(int n) {
+
+  //}
+  //public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+
+  //}
+}
